8060967b95c72e362a606f096cbfcfe31a982b23
abdu <etabdi@yahoo.com>
2020-02-12 21:59:52 -0600

    update skill

--- public/contactform/contactform.php
+++ public/contactform/contactform.php
@@ -27,7 +27,7 @@
     $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
     $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
 
-    if(!$contact_email_to || $contact_email_to == 'contact@example.com') {
+    if(!$contact_email_to || $contact_email_to == 'etabdi@yahoo.com') {
       die('The contact form receiving email address is not configured!');
     }
 
